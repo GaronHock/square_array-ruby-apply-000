87ddfaab3269a67a74ea0f2f4a2c4537406dc9e5
wise-anchor-7998 <garonhock@yahoo.com>
2019-09-13 17:11:15 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,10 @@
 def square_array(array)
-  square_array = []
+  
   numbers = [1,2,3]
    numbers.each do |ele|
-     square_array.push(ele * ele)
+     ele * ele
   end
-  return square_array
+  return numbers
   
 
   
