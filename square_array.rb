4e861607d759df01e26cfa7542da07ea3fed6c31
wise-anchor-7998 <garonhock@yahoo.com>
2019-09-13 17:08:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,20 @@
 def square_array(array)
-  # your code here
+  square_array = []
+  another_square_array = []
+  numbers = [1,2,3]
+   i = 0 
+   while i < numbers.length 
+   square_array << numbers[i] * numbers[i]
+   i += 1
+   
+  end
+  return square_array
+  
+  numbers.each { |ele| square_array << ele }
+  return square_array
+  
+  
+  
+    
+
 end
